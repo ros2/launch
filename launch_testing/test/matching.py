f23c341768397c0fc12a7190a71890c92c5f6fb9
dhood <deanna.m.hood@gmail.com>
2016-06-01 12:54:03 -0700

    Add option to repeat core lines n times

--- launch_testing/test/matching.py
+++ launch_testing/test/matching.py
@@ -24,17 +24,20 @@ def main():
     parser.add_argument('--prepended-lines', action='store_true')
     parser.add_argument('--appended-lines', action='store_true')
     parser.add_argument('--interleaved-lines', action='store_true')
+    parser.add_argument('--repeated-times', action='store', type=int, default=0)
     args = parser.parse_args()
 
     if not args.no_output:
         if args.prepended_lines:
             print('license output', file=sys.stdout)
-        print('this is line 1', file=sys.stdout)
 
-        if args.interleaved_lines:
-            print('debug output', file=sys.stdout)
+        for i in range(args.repeated_times + 1):
+            print('this is line 1', file=sys.stdout)
 
-        print('this is line b', file=sys.stdout)
+            if args.interleaved_lines:
+                print('debug output', file=sys.stdout)
+
+            print('this is line b', file=sys.stdout)
 
         if args.appended_lines:
             print('extra printout', file=sys.stdout)
