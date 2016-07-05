961a90334f48894a14d9470575340bb9aaa616cc
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-07-05 13:08:43 -0700

    use deepcopy to be sure to use disctinct dictionaries

--- launch_testing/test/check_env.py
+++ launch_testing/test/check_env.py
@@ -1,4 +1,4 @@
-import os
-
-assert(os.environ.get('testenv2', 'Not Set') == 'Not Set')
-assert(os.environ.get('testenv1', 'Not Set') == 'testval1')
+if __name__ == '__main__':
+    import os
+    assert(os.environ.get('testenv2', 'Not Set') == 'Not Set')
+    assert(os.environ.get('testenv1', 'Not Set') == 'testval1')
