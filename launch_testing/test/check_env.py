f49d6b681fa3c475976730e42f04b1d1a34de890
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-07-05 13:23:48 -0700

    move import to module level, remove unused brackets assert

--- launch_testing/test/check_env.py
+++ launch_testing/test/check_env.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 
 if __name__ == '__main__':
-    import os
-    assert(os.environ.get('testenv2', 'Not Set') == 'Not Set')
-    assert(os.environ.get('testenv1', 'Not Set') == 'testval1')
+    assert os.environ.get('testenv2', 'Not Set') == 'Not Set'
+    assert os.environ.get('testenv1', 'Not Set') == 'testval1'
