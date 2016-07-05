49466a414e51d49a4e59848a8a1747a1f4c43b1a
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-07-05 12:49:30 -0700

    add unit test

---
+++ launch_testing/test/check_env.py
@@ -0,0 +1,4 @@
+import os
+
+assert(os.environ.get('testenv2', 'Not Set') == 'Not Set')
+assert(os.environ.get('testenv1', 'Not Set') == 'testval1')
---
+++ launch_testing/test/test_env_passing.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from launch import LaunchDescriptor
+from launch.launcher import DefaultLauncher
+from launch.exit_handler import primary_exit_handler
+
+
+def test_env():
+    ld = LaunchDescriptor()
+
+    sub_env = os.environ
+    sub_env['testenv1'] = 'testval1'
+#    os.environ['testenv2'] = 'testval2'
+    ld.add_process(
+        cmd=[
+            sys.executable,
+            os.path.join(
+                os.path.abspath(
+                    os.path.dirname(
+                        os.path.dirname(
+                            os.path.dirname(__file__)))),
+                'check_env.py')],
+        name='test_env',
+        env=sub_env,
+        exit_handler=primary_exit_handler,
+    )
+    launcher = DefaultLauncher()
+    launcher.add_launch_descriptor(ld)
+    rc = launcher.launch()
+
+    assert rc == 0, \
+        "The launch file failed with exit code '" + str(rc) + "'. "
+
+
+if __name__ == '__main__':
+    test_env()
