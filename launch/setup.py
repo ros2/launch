1cf60f1f3bef03986a4b69f39571c364b6241fa8
William Woodall <william@osrfoundation.org>
2018-06-06 01:05:34 -0700

    fix missing find/replace for launch->launch.legacy

--- launch/setup.py
+++ launch/setup.py
@@ -28,7 +28,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'launch = launch.main:main',
+            'launch = launch.legacy.main:main',
         ],
     },
 )
