093c706aaf742f2374916370154f939c30edb206
Mikael Arguedas <mikael@osrfoundation.org>
2018-06-15 10:51:48 +0200

    fixup alpha linter label

--- ros2launch/test/test_pep257.py
+++ ros2launch/test/test_pep257.py
@@ -16,8 +16,8 @@ from ament_pep257.main import main
 import pytest
 
 
-@pytest.mark.pep257
 @pytest.mark.linter
+@pytest.mark.pep257
 def test_pep257():
     rc = main(argv=[])
     assert rc == 0, 'Found code style errors / warnings'
