6213656d003562cd5d42dd7a92f0ad573758c4cc
William Woodall <william@osrfoundation.org>
2018-08-14 12:06:24 -0700

    add ability to pass arguments when including a launch description

--- launch/launch/actions/include_launch_description.py
+++ launch/launch/actions/include_launch_description.py
@@ -15,26 +15,74 @@
 """Module for the IncludeLaunchDescription action."""
 
 import os
+from typing import Iterable
 from typing import List
+from typing import Optional
+from typing import Tuple
 
+from .pop_launch_configurations import PopLaunchConfigurations
+from .push_launch_configurations import PushLaunchConfigurations
+from .set_launch_configuration import SetLaunchConfiguration
 from ..action import Action
 from ..launch_context import LaunchContext
 from ..launch_description_entity import LaunchDescriptionEntity
 from ..launch_description_source import LaunchDescriptionSource
+from ..some_substitutions_type import SomeSubstitutionsType
+from ..utilities import normalize_to_list_of_substitutions
+from ..utilities import perform_substitutions
 
 
 class IncludeLaunchDescription(Action):
-    """Action that includes a launch description source and yields its entities when visited."""
+    """
+    Action that includes a launch description source and yields its entities when visited.
 
-    def __init__(self, launch_description_source: LaunchDescriptionSource) -> None:
+    It is possible to pass arguments to the launch description, which it
+    declared with the :py:class:`launch.actions.DeclareLaunchArgument` action.
+
+    If any given arguments do not match the name of any declared launch
+    arguments, then they will still be set as Launch Configurations using the
+    :py:class:`launch.actions.SetLaunchConfiguration` action.
+    This is done because it's not always possible to detect all instances of
+    the declare launch argument class in the given launch description.
+
+    On the other side, an error will sometimes be raised if the given launch
+    description declares a launch argument and its value is not provided to
+    this action.
+    It will only produce this error, however, if the declared launch argument
+    is unconditional (sometimes the action that declares the launch argument
+    will only be visited in certain circumstances) and if it does not have a
+    default value on which to fall back.
+
+    Conditionally included launch arguments that do not have a default value
+    will eventually raise an error if this best effort argument checking is
+    unable to see an unsatisfied argument ahead of time.
+    """
+
+    def __init__(
+        self,
+        launch_description_source: LaunchDescriptionSource,
+        *,
+        launch_arguments: Optional[
+            Iterable[Tuple[SomeSubstitutionsType, SomeSubstitutionsType]]
+        ] = None,
+    ) -> None:
         """Constructor."""
         super().__init__()
         self.__launch_description_source = launch_description_source
+        self.__launch_arguments = launch_arguments
 
     @property
     def launch_description_source(self) -> LaunchDescriptionSource:
         """Getter for self.__launch_description_source."""
         return self.__launch_description_source
+
+    @property
+    def launch_arguments(self) -> Iterable[Tuple[SomeSubstitutionsType, SomeSubstitutionsType]]:
+        """Getter for self.__launch_arguments."""
+        if self.__launch_arguments is None:
+            return []
+        else:
+            return self.__launch_arguments
 
     def visit(self, context: LaunchContext) -> List[LaunchDescriptionEntity]:
         """Override visit to return an Entity rather than an action."""
@@ -49,4 +97,33 @@ class IncludeLaunchDescription(Action):
         context.extend_locals({
             'current_launch_file_directory': launch_file_location,
         })
-        return [launch_description]
+
+        # Do best effort checking to see if non-optional, non-default declared arguments
+        # are being satisfied.
+        argument_names = [
+            perform_substitutions(context, normalize_to_list_of_substitutions(arg_name))
+            for arg_name, arg_value in self.launch_arguments
+        ]
+        declared_launch_arguments = launch_description.get_launch_arguments()
+        for argument in declared_launch_arguments:
+            if argument._conditionally_included or argument.default_value is not None:
+                continue
+            if argument.name not in argument_names:
+                raise RuntimeError(
+                    "Included launch description missing required argument '{}' "
+                    "(description: '{}'), given: [{}]"
+                    .format(argument.name, argument.description, ', '.join(argument_names))
+                )
+
+        # Create actions to set the launch arguments into the launch configurations.
+        set_launch_configuration_actions = []
+        for name, value in self.launch_arguments:
+            set_launch_configuration_actions.append(SetLaunchConfiguration(name, value))
+
+        # Push configs, set arguments as configs, include launch description, pop configs.
+        return [
+            PushLaunchConfigurations(),
+            *set_launch_configuration_actions,
+            launch_description,
+            PopLaunchConfigurations()
+        ]
