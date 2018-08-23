c236b57f54bf16d117eac8fd76e1f937d427d452
William Woodall <william@osrfoundation.org>
2018-08-22 19:57:07 -0700

    test new features of LaunchDescription class
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- launch/test/launch/test_launch_description.py
+++ launch/test/launch/test_launch_description.py
@@ -15,10 +15,20 @@
 """Tests for the LaunchDescription class."""
 
 import collections
+import logging
+import os
 
 from launch import Action
 from launch import LaunchDescription
 from launch import LaunchDescriptionEntity
+from launch import LaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+
+logging.getLogger('launch').setLevel(logging.DEBUG)
 
 
 def test_launch_description_constructors():
@@ -28,6 +38,50 @@ def test_launch_description_constructors():
     LaunchDescription([])
     ld = LaunchDescription([LaunchDescriptionEntity()])
     assert len(ld.entities) == 1
+
+
+def test_launch_description_get_launch_arguments():
+    """Test the get_launch_arguments() method of the LaunchDescription class."""
+    ld = LaunchDescription([])
+    assert len(ld.get_launch_arguments()) == 0
+
+    ld = LaunchDescription([DeclareLaunchArgument('foo')])
+    la = ld.get_launch_arguments()
+    assert len(la) == 1
+    assert la[0]._conditionally_included is False
+
+    ld = LaunchDescription([DeclareLaunchArgument('foo', condition=IfCondition('True'))])
+    la = ld.get_launch_arguments()
+    assert len(la) == 1
+    assert la[0]._conditionally_included is True
+
+    ld = LaunchDescription([
+        IncludeLaunchDescription(LaunchDescriptionSource(LaunchDescription([
+            DeclareLaunchArgument('foo'),
+        ]))),
+    ])
+    la = ld.get_launch_arguments()
+    assert len(la) == 1
+    assert la[0]._conditionally_included is False
+
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    ld = LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            os.path.join(this_dir, 'launch_file_with_argument.launch.py'))),
+    ])
+    la = ld.get_launch_arguments()
+    assert len(la) == 1
+    assert la[0]._conditionally_included is False
+
+    ld = LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource([
+            # This will prevent loading of this launch file to find arguments in it.
+            ThisLaunchFileDir(),
+            'launch_file_with_argument.launch.py',
+        ])),
+    ])
+    la = ld.get_launch_arguments()
+    assert len(la) == 0
 
 
 def test_launch_description_add_things():
