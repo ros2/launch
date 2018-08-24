4f8ee9b83498af6be236d95ab04123aed061d579
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-08-24 10:18:54 -0700

    add legacy imports to maintain backward compatibility

---
+++ launch_testing/launch_testing/__init__.py
@@ -0,0 +1,7 @@
+# for backward compatibility
+from launch_testing.legacy import InMemoryHandler  # noqa: F401
+from launch_testing.legacy import UnmatchedOutputError  # noqa: F401
+from launch_testing.legacy import create_handler  # noqa: F401
+from launch_testing.legacy import get_default_filtered_patterns  # noqa: F401
+from launch_testing.legacy import get_default_filtered_prefixes  # noqa: F401
+from launch_testing.legacy import get_rmw_output_filter  # noqa: F401
