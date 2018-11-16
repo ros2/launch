0360b751d8680f6e0ca86ffbbf838cb5fcded882
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-11-16 07:57:01 -0800

    Missed it by that much.
    Between creating this branch and submitting it a release happened.

--- launch/setup.py
+++ launch/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='launch',
-    version='0.6.0',
+    version='0.7.0',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     zip_safe=True,
