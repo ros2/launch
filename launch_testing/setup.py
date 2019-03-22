8fd3470068eebe695c33666b9544fbca3eb51a1a
Michel Hidalgo <michel@ekumenlabs.com>
2019-03-22 12:28:07 -0300

    Make launch_testing a pure Python package again.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ launch_testing/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='launch_testing',
+    version='0.7.3',
+    packages=find_packages(exclude=['test']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Esteve Fernandez',
+    author_email='esteve@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='https://github.com/ros2/launch',
+    download_url='https://github.com/ros2/launch/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Test the output of a process.',
+    long_description='ROS launch testing suite.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+)
