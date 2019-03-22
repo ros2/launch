853bb8bc6df1b9a20c0de9eae3b2d9314992ad44
Michel Hidalgo <michel@ekumenlabs.com>
2019-03-22 12:41:42 -0300

    Merge apex_launchtest into launch_testing.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- launch_testing/setup.py
+++ launch_testing/setup.py
@@ -1,16 +1,24 @@
+import glob
 from setuptools import find_packages
 from setuptools import setup
+
 
 setup(
     name='launch_testing',
     version='0.7.3',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/launch_testing']),
+        ('lib/launch_testing', glob.glob('example_processes/**')),
+        ('share/launch_testing/examples', glob.glob('examples/[!_]**')),
+        ('bin', ['scripts/launchtest']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    author='Esteve Fernandez',
-    author_email='esteve@osrfoundation.org',
-    maintainer='Dirk Thomas',
-    maintainer_email='dthomas@osrfoundation.org',
+    author='Pete Baughman',
+    author_email='pete.baughman@apex.ai',
+    maintainer='Pete Baughman',
+    maintainer_email='pete.baughman@apex.ai',
     url='https://github.com/ros2/launch',
     download_url='https://github.com/ros2/launch/releases',
     keywords=['ROS'],
