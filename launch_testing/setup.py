68b0f06901aa882b0c085621463f399513bb5fe9
Michel Hidalgo <michel@ekumenlabs.com>
2019-03-26 12:18:20 -0300

    Apply some rectifications to package.xml and setup.py files.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- launch_testing/setup.py
+++ launch_testing/setup.py
@@ -29,8 +29,8 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description='Test the output of a process.',
-    long_description='ROS launch testing suite.',
+    description='Create tests which involve launch files and multiple processes.',
+    long_description='A package to create tests which involve launch files and multiple processes.',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
 )
