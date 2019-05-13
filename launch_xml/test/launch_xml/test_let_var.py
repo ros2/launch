70891217e7a5318e1a03ec411ec27138e73ce080
ivanpauno <ivanpauno@ekumenlabs.com>
2019-05-13 18:00:55 -0300

    Add test for let action and var substitution
    Signed-off-by: ivanpauno <ivanpauno@ekumenlabs.com>

---
+++ launch_xml/test/launch_xml/test_let_var.py
@@ -0,0 +1,37 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test parsing an executable action."""
+
+from pathlib import Path
+
+from launch import LaunchContext
+
+from launch_frontend import Parser
+
+
+def test_let_var():
+    """Parse node xml example."""
+    root_entity, parser = Parser.load(str(Path(__file__).parent / 'let_var.xml'))
+    ld = parser.parse_description(root_entity)
+    context = LaunchContext()
+    assert len(ld.entities) == 2
+    ld.entities[0].execute(context)
+    ld.entities[1].execute(context)
+    assert context.launch_configurations['var1'] == 'asd'
+    assert context.launch_configurations['var2'] == '2 asd'
+
+
+if __name__ == '__main__':
+    test_let_var()
