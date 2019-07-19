3b2932111b711c8640765df6f4b45cfa007b52bc
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-07-19 13:04:54 -0700

    pass preferred encoding to decode()
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- launch/launch/logging/__init__.py
+++ launch/launch/logging/__init__.py
@@ -335,7 +335,8 @@ def get_screen_handler():
     if launch_config.screen_handler is None:
         stream = codecs.StreamWriter(sys.stdout, errors='replace')
         stream.encode = lambda msg, errors='replace': (
-            msg.encode(locale.getpreferredencoding(False), errors).decode(),
+            msg.encode(locale.getpreferredencoding(False), errors).decode(
+                locale.getpreferredencoding(False), errors=errors),
             msg)
         launch_config.screen_handler = handlers.StreamHandler(stream)
         launch_config.screen_handler.setFormatter(launch_config.screen_formatter)
