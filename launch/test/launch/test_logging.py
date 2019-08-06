c8007cb1a0e0057c45dab87e8e1c2da52e2afd24
Chris Lalancette <clalancette@openrobotics.org>
2019-08-06 19:39:15 +0000

    Don't create a log directory every time the launch logger is imported.
    To do this, we revamp the launch_config() function into a class
    with member variables and methods.  We then make the 'log_dir'
    a property, and if it is None when it is accessed, we create
    the directory then.  This ensures that we will not create the
    directory until someone actually wants to use it.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- launch/test/launch/test_logging.py
+++ launch/test/launch/test_logging.py
@@ -34,20 +34,20 @@ def test_bad_logging_launch_config():
     launch.logging.reset()
 
     with pytest.raises(ValueError):
-        launch.logging.launch_config(log_dir='not/a/real/dir')
+        launch.logging.launch_config.configure(log_dir='not/a/real/dir')
 
     with pytest.raises(ValueError):
-        launch.logging.launch_config(screen_format='default', screen_style='%')
+        launch.logging.launch_config.configure(screen_format='default', screen_style='%')
 
     with pytest.raises(ValueError):
-        launch.logging.launch_config(log_format='default', log_style='%')
+        launch.logging.launch_config.configure(log_format='default', log_style='%')
 
 
 def test_output_loggers_bad_configuration(log_dir):
     """Tests that output loggers setup throws at bad configuration."""
-    launch.logging.reset()
+    launch.logging.launch_config.reset()
 
-    launch.logging.launch_config(log_dir=log_dir)
+    launch.logging.launch_config.configure(log_dir=log_dir)
 
     with pytest.raises(ValueError):
         launch.logging.get_output_loggers('some-proc', 'not_an_alias')
@@ -84,15 +84,15 @@ def test_output_loggers_bad_configuration(log_dir):
 def test_output_loggers_configuration(capsys, log_dir, config, checks):
     checks = {'stdout': set(), 'stderr': set(), 'both': set(), **checks}
     launch.logging.reset()
-    launch.logging.launch_config(
+    launch.logging.launch_config.configure(
         level=logging.INFO,
         log_dir=log_dir,
         screen_format='default',
         log_format='default'
     )
     logger = launch.logging.get_logger('some-proc')
-    logger.addHandler(launch.logging.get_screen_handler())
-    logger.addHandler(launch.logging.get_log_file_handler())
+    logger.addHandler(launch.logging.launch_config.get_screen_handler())
+    logger.addHandler(launch.logging.launch_config.get_log_file_handler())
     logger.setLevel(logging.ERROR)
     stdout_logger, stderr_logger = launch.logging.get_output_loggers('some-proc', config)
 
@@ -111,8 +111,8 @@ def test_output_loggers_configuration(capsys, log_dir, config, checks):
     assert 0 == len(lines)
     assert 0 == len(capture.err)
 
-    launch.logging.get_log_file_handler().flush()
-    main_log_path = launch.logging.get_log_file_path()
+    launch.logging.launch_config.get_log_file_handler().flush()
+    main_log_path = launch.logging.launch_config.get_log_file_path()
     assert os.path.exists(main_log_path)
     assert 0 != os.stat(main_log_path).st_size
     with open(main_log_path, 'r') as f:
@@ -125,8 +125,8 @@ def test_output_loggers_configuration(capsys, log_dir, config, checks):
         assert 0 == len(lines)
 
     if 'own_log' in (checks['stdout'] | checks['both']):
-        launch.logging.get_log_file_handler('some-proc-stdout.log').flush()
-        own_log_path = launch.logging.get_log_file_path('some-proc-stdout.log')
+        launch.logging.launch_config.get_log_file_handler('some-proc-stdout.log').flush()
+        own_log_path = launch.logging.launch_config.get_log_file_path('some-proc-stdout.log')
         assert os.path.exists(own_log_path)
         assert 0 != os.stat(own_log_path).st_size
         with open(own_log_path, 'r') as f:
@@ -134,12 +134,12 @@ def test_output_loggers_configuration(capsys, log_dir, config, checks):
             assert 1 == len(lines)
             assert 'foo' == lines[0]
     else:
-        own_log_path = launch.logging.get_log_file_path('some-proc-stdout.log')
+        own_log_path = launch.logging.launch_config.get_log_file_path('some-proc-stdout.log')
         assert (not os.path.exists(own_log_path) or 0 == os.stat(own_log_path).st_size)
 
     if 'own_log' in (checks['stderr'] | checks['both']):
-        launch.logging.get_log_file_handler('some-proc-stderr.log').flush()
-        own_log_path = launch.logging.get_log_file_path('some-proc-stderr.log')
+        launch.logging.launch_config.get_log_file_handler('some-proc-stderr.log').flush()
+        own_log_path = launch.logging.launch_config.get_log_file_path('some-proc-stderr.log')
         assert os.path.exists(own_log_path)
         assert 0 != os.stat(own_log_path).st_size
         with open(own_log_path, 'r') as f:
@@ -147,12 +147,12 @@ def test_output_loggers_configuration(capsys, log_dir, config, checks):
             assert 1 == len(lines)
             assert 'bar' == lines[0]
     else:
-        own_log_path = launch.logging.get_log_file_path('some-proc-stderr.log')
+        own_log_path = launch.logging.launch_config.get_log_file_path('some-proc-stderr.log')
         assert (not os.path.exists(own_log_path) or 0 == os.stat(own_log_path).st_size)
 
     if 'own_log' in checks['both']:
-        launch.logging.get_log_file_handler('some-proc.log').flush()
-        own_log_path = launch.logging.get_log_file_path('some-proc.log')
+        launch.logging.launch_config.get_log_file_handler('some-proc.log').flush()
+        own_log_path = launch.logging.launch_config.get_log_file_path('some-proc.log')
         assert os.path.exists(own_log_path)
         assert 0 != os.stat(own_log_path).st_size
         with open(own_log_path, 'r') as f:
@@ -161,20 +161,20 @@ def test_output_loggers_configuration(capsys, log_dir, config, checks):
             assert 'foo' == lines[0]
             assert 'bar' == lines[1]
     else:
-        own_log_path = launch.logging.get_log_file_path('some-proc.log')
+        own_log_path = launch.logging.launch_config.get_log_file_path('some-proc.log')
         assert (not os.path.exists(own_log_path) or 0 == os.stat(own_log_path).st_size)
 
 
 def test_screen_default_format_with_timestamps(capsys, log_dir):
     """Test screen logging when using the default logs format with timestamps."""
     launch.logging.reset()
-    launch.logging.launch_config(
+    launch.logging.launch_config.configure(
         level=logging.DEBUG,
         log_dir=log_dir,
         screen_format='default_with_timestamp',
     )
     logger = launch.logging.get_logger('some-proc')
-    logger.addHandler(launch.logging.get_screen_handler())
+    logger.addHandler(launch.logging.launch_config.get_screen_handler())
     assert logger.getEffectiveLevel() == logging.DEBUG
 
     logger.debug('foo')
@@ -189,13 +189,13 @@ def test_screen_default_format_with_timestamps(capsys, log_dir):
 def test_screen_default_format(capsys):
     """Test screen logging when using the default logs format."""
     launch.logging.reset()
-    launch.logging.launch_config(
+    launch.logging.launch_config.configure(
         level=logging.INFO,
         screen_format='default'
     )
 
     logger = launch.logging.get_logger('some-proc')
-    logger.addHandler(launch.logging.get_screen_handler())
+    logger.addHandler(launch.logging.launch_config.get_screen_handler())
     assert logger.getEffectiveLevel() == logging.INFO
 
     logger.info('bar')
@@ -209,22 +209,22 @@ def test_screen_default_format(capsys):
 def test_log_default_format(log_dir):
     """Test logging to the main log file when using the default logs format."""
     launch.logging.reset()
-    launch.logging.launch_config(
+    launch.logging.launch_config.configure(
         level=logging.WARN,
         log_dir=log_dir,
         log_format='default'
     )
     logger = launch.logging.get_logger('some-proc')
-    logger.addHandler(launch.logging.get_log_file_handler())
+    logger.addHandler(launch.logging.launch_config.get_log_file_handler())
     assert logger.getEffectiveLevel() == logging.WARN
 
     logger.error('baz')
 
-    launch.logging.get_log_file_handler().flush()
-    assert os.path.exists(launch.logging.get_log_file_path())
-    assert 0 != os.stat(launch.logging.get_log_file_path()).st_size
+    launch.logging.launch_config.get_log_file_handler().flush()
+    assert os.path.exists(launch.logging.launch_config.get_log_file_path())
+    assert 0 != os.stat(launch.logging.launch_config.get_log_file_path()).st_size
 
-    with open(launch.logging.get_log_file_path(), 'r') as f:
+    with open(launch.logging.launch_config.get_log_file_path(), 'r') as f:
         lines = f.readlines()
         assert 1 == len(lines)
         assert re.match(r'[0-9]+\.[0-9]+ \[ERROR\] \[some-proc\]: baz', lines[0]) is not None
@@ -245,7 +245,7 @@ def test_log_handler_factory(log_dir):
     outputs = collections.defaultdict(list)
 
     launch.logging.reset()
-    launch.logging.launch_config(
+    launch.logging.launch_config.configure(
         level=logging.WARN,
         log_dir=log_dir,
         log_format='default',
@@ -257,12 +257,12 @@ def test_log_handler_factory(log_dir):
     )
 
     logger = launch.logging.get_logger('some-proc')
-    logger.addHandler(launch.logging.get_log_file_handler())
+    logger.addHandler(launch.logging.launch_config.get_log_file_handler())
 
     logger.debug('foo')
     logger.error('baz')
 
-    path = launch.logging.get_log_file_path()
+    path = launch.logging.launch_config.get_log_file_path()
     assert path in outputs
     assert len(outputs[path]) == 1
     assert outputs[path][0].endswith('baz')
