7ab8dc3ef322e40e681a78c270f2109fefca7347
Dan Rose <dan@digilabs.io>
2020-05-14 12:58:32 -0500

    Rearrange copyright
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- launch/launch/frontend/parser.py
+++ launch/launch/frontend/parser.py
@@ -1,5 +1,5 @@
-# Copyright 2020 Rover Robotics, c/o Dan Rose
 # Copyright 2019 Open Source Robotics Foundation, Inc.
+# Copyright 2020 Rover Robotics, c/o Dan Rose
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
