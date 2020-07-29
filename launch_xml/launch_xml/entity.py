2159a5a4992ab0f52c64dfd8b9f657aab6380e67
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-07-29 17:21:35 -0300

    Consolidate type_utils in a way that can be reused in substitution results that need to be coerced to a specific type (#438)
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- launch_xml/launch_xml/entity.py
+++ launch_xml/launch_xml/entity.py
@@ -14,7 +14,6 @@
 
 """Module for Entity class."""
 
-from typing import Any
 from typing import List
 from typing import Optional
 from typing import Text
@@ -23,7 +22,9 @@ import xml.etree.ElementTree as ET
 
 from launch.frontend import Entity as BaseEntity
 from launch.frontend.type_utils import check_is_list_entity
-from launch.frontend.type_utils import get_typed_value
+from launch.utilities.type_utils import AllowedTypesType
+from launch.utilities.type_utils import AllowedValueType
+from launch.utilities.type_utils import get_typed_value
 
 
 class Entity(BaseEntity):
@@ -58,14 +59,20 @@ class Entity(BaseEntity):
         self,
         name: Text,
         *,
-        data_type: Any = str,
-        optional: bool = False
+        data_type: AllowedTypesType = str,
+        optional: bool = False,
+        can_be_str: bool = True,
     ) -> Optional[Union[
-        List[Union[int, str, float, bool]],
-        Union[int, str, float, bool],
-        List['Entity']
+        AllowedValueType,
+        List['Entity'],
     ]]:
-        """Access an attribute of the entity."""
+        """
+        Access an attribute of the entity.
+
+        See :ref:meth:`launch.frontend.Entity.get_attr`.
+        `launch_xml` uses type coercion.
+        If coercion fails, `ValueError` will be raised.
+        """
         attr_error = AttributeError(
             'Attribute {} of type {} not found in Entity {}'.format(
                 name, data_type, self.type_name
@@ -93,7 +100,7 @@ class Entity(BaseEntity):
             else:
                 return None
         try:
-            value = get_typed_value(value, data_type)
+            value = get_typed_value(value, data_type, can_be_str=can_be_str)
         except ValueError:
             raise TypeError(
                 'Attribute {} of Entity {} expected to be of type {}.'
