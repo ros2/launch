4bf31f4976b641745bb664ce45251eef5f34e52c
matthew.lanting <mlanting@dcscorp.com>
2021-08-02 15:15:19 -0400

    Removed some language from file headers
    Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    
    Signed-off-by: matthew.lanting <mlanting@dcscorp.com>

--- launch/test/launch/test_execute_local.py
+++ launch/test/launch/test_execute_local.py
@@ -14,11 +14,6 @@
 #
 # DISTRIBUTION A. Approved for public release; distribution unlimited.
 # OPSEC #4584.
-#
-# Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
-# Part 252.227-7013 or 7014 (Feb 2014).
-#
-# This notice must appear in all copies of this file and its derivatives.
 
 """Tests for the ExecuteLocal Action."""
 
