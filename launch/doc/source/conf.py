94ecffc07bee70384d34294838dc91464f30da1b
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2021-08-23 18:17:25 -0300

    Sphinx update conf.py
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- launch/doc/source/conf.py
+++ launch/doc/source/conf.py
@@ -64,6 +64,13 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+# autodoc settings
+autodoc_default_options = {
+    'special-members': '__init__',
+    'class-doc-from': 'class',
+}
+autodoc_class_signature = 'separated'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -108,7 +115,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
