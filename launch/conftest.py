fa7896adb7affedaf5e569efbd094b69db0bf32f
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2021-08-25 13:27:58 -0300

    Ignore problematic files
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- launch/conftest.py
+++ launch/conftest.py
@@ -13,16 +13,14 @@
 # limitations under the License.
 
 # imports needed for doctests
-import launch
-from launch import LaunchDescription
-import launch.actions
-
-import pytest
+# import pytest
 
 
-@pytest.fixture(autouse=True)
-def add_imports_to_doctest_namespace(doctest_namespace):
-    doctest_namespace['launch'] = launch
-    doctest_namespace['LaunchDescription'] = LaunchDescription
-    for x in launch.actions.__all__:
-        doctest_namespace[x] = getattr(launch.actions, x)
+def pytest_ignore_collect(path):
+    # pytest doctest messes up when trying to import .launch.py packages, ignore them.
+    # It also messes up when trying to import launch.logging.handlers due to conflicts with
+    # logging.handlers, ignore that as well.
+    return str(path).endswith((
+        '.launch.py',
+        'logging/handlers.py',
+    ))
