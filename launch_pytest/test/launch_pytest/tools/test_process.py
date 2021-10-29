0279cf7f53f2f1268deb5da965211a7eeb4c7f03
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2021-10-29 15:46:23 -0300

    fix windows
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- launch_pytest/test/launch_pytest/tools/test_process.py
+++ launch_pytest/test/launch_pytest/tools/test_process.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import sys
 
 import launch
@@ -49,11 +50,11 @@ def launch_description(dut):
 @pytest.mark.launch(fixture=launch_description)
 async def test_async_process_tools(dut, launch_context):
     await tools.wait_for_start(launch_context, dut, timeout=10)
-    def check_output(output): assert output == 'hello\n'
+    def check_output(output): assert output == f'hello{os.linesep}'
     await tools.wait_for_output(
         launch_context, dut, check_output, timeout=10)
 
-    def check_stderr(err): assert err == 'world\n'
+    def check_stderr(err): assert err == f'world{os.linesep}'
     await tools.wait_for_stderr(
         launch_context, dut, check_stderr, timeout=10)
     await tools.wait_for_exit(launch_context, dut, timeout=10)
@@ -62,11 +63,11 @@ async def test_async_process_tools(dut, launch_context):
 @pytest.mark.launch(fixture=launch_description)
 def test_sync_process_tools(dut, launch_context):
     tools.wait_for_start_sync(launch_context, dut, timeout=10)
-    def check_output(output): assert output == 'hello\n'
+    def check_output(output): assert output == f'hello{os.linesep}'
     tools.wait_for_output_sync(
         launch_context, dut, check_output, timeout=10)
 
-    def check_stderr(err): assert err == 'world\n'
+    def check_stderr(err): assert err == f'world{os.linesep}'
     tools.wait_for_stderr_sync(
         launch_context, dut, check_stderr, timeout=10)
     tools.wait_for_exit_sync(launch_context, dut, timeout=10)
