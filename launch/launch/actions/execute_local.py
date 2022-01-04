32dbb67a5b74e67c62483c213d3346906d64669e
Scott K Logan <logans@cottsay.net>
2022-01-04 11:14:57 -0800

    Use asyncio.wait with timeout rather than sleep (#576)
    The latter approach seems to hang with Python 3.6.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- launch/launch/actions/execute_local.py
+++ launch/launch/actions/execute_local.py
@@ -574,10 +574,9 @@ class ExecuteLocal(Action):
                 # wait for a timeout(`self.__respawn_delay`) to respawn the process
                 # and handle shutdown event with future(`self.__shutdown_future`)
                 # to make sure `ros2 launch` exit in time
-                respawn_task = asyncio.create_task(asyncio.sleep(self.__respawn_delay))
                 await asyncio.wait(
-                    [respawn_task, self.__shutdown_future],
-                    return_when=asyncio.FIRST_COMPLETED
+                    (self.__shutdown_future,),
+                    timeout=self.__respawn_delay
                 )
             if not self.__shutdown_future.done():
                 context.asyncio_loop.create_task(self.__execute_process(context))
