d2935d6fd85636ace62699eec106cd56f5e9c6a4
Aditya Pande <aditya050995@gmail.com>
2022-04-15 17:40:01 -0700

    Expose shutdown action to xml frontend (#611)
    * expose shutdown action to xml frontend
    
    Signed-off-by: Aditya <aditya050995@gmail.com>

--- launch/launch/actions/shutdown_action.py
+++ launch/launch/actions/shutdown_action.py
@@ -17,6 +17,10 @@
 import logging
 from typing import Text
 
+from launch.frontend import Entity
+from launch.frontend import expose_action
+from launch.frontend import Parser
+
 from .emit_event import EmitEvent
 from ..events import Shutdown as ShutdownEvent
 from ..events.process import ProcessExited
@@ -25,11 +29,21 @@ from ..launch_context import LaunchContext
 _logger = logging.getLogger(name='launch')
 
 
+@expose_action('shutdown')
 class Shutdown(EmitEvent):
     """Action that shuts down a launched system by emitting Shutdown when executed."""
 
     def __init__(self, *, reason: Text = 'reason not given', **kwargs):
         super().__init__(event=ShutdownEvent(reason=reason), **kwargs)
+
+    @classmethod
+    def parse(cls, entity: Entity, parser: Parser):
+        """Return `Shutdown` action and kwargs for constructing it."""
+        _, kwargs = super().parse(entity, parser)
+        reason = entity.get_attr('reason', optional=True)
+        if reason:
+            kwargs['reason'] = parser.parse_substitution(reason)
+        return cls, kwargs
 
     def execute(self, context: LaunchContext):
         """Execute the action."""
