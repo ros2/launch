e45bed9f5a185f0e9013f991bb7e3a10ac4b8e90
William Woodall <william@osrfoundation.org>
2023-09-19 18:57:02 -0700

    add missing test executable
    Signed-off-by: William Woodall <william@osrfoundation.org>

---
+++ launch/test/launch/argv_echo.py
@@ -0,0 +1,28 @@
+# Copyright 2023 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Used from test_execute_process.py and others."""
+
+
+import sys
+
+
+def print_argv(argv):
+    joined_args = "', '".join(argv)
+    print(f"['{joined_args}']")
+    sys.exit(len(argv))
+
+
+if __name__ == '__main__':
+    print_argv(sys.argv)
