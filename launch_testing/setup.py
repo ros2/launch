eea42eb2948a7ba9d893b50a9dc7a0006d4be632
Marco A. Gutierrez <marcogg@marcogg.com>
2024-04-26 15:58:24 +0000

    3.5.0

--- launch_testing/setup.py
+++ launch_testing/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='launch_testing',
-    version='3.4.2',
+    version='3.5.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/launch_testing']),
