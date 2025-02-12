a4a4c840b2280b09ee52646c07e96088eb9ac2c3
Roland Arsenault <roland@rolker.net>
2025-02-12 09:11:21 -0500

    Update launch/launch/actions/include_launch_description.py
    Co-authored-by: Christophe Bedard <bedard.christophe@gmail.com>
    Signed-off-by: Roland Arsenault <roland@rolker.net>

--- launch/launch/actions/include_launch_description.py
+++ launch/launch/actions/include_launch_description.py
@@ -166,7 +166,7 @@ class IncludeLaunchDescription(Action):
                 launch_description.get_launch_arguments_with_include_launch_description_actions())
         except Exception as exc:
             if hasattr(exc, 'add_note'):
-                exc.add_note(f'while executing {self.describe()}')
+                exc.add_note(f'while executing {self.describe()}')  # type: ignore
             raise
         for argument, ild_actions in declared_launch_arguments:
             if argument._conditionally_included or argument.default_value is not None:
