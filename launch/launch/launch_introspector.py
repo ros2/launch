7302368979e844547563840a0e7b2e20ef6e281c
Michael Carlstrom <rmc@carlstrom.com>
2025-02-24 18:49:29 -0500

    Fix ament_flake8
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- launch/launch/launch_introspector.py
+++ launch/launch/launch_introspector.py
@@ -114,7 +114,8 @@ def format_action(action: Action) -> List[Text]:
                 format_substitutions(typed_action.cwd)
             ),
             typed_action.env if typed_action.env is None else '{' + ', '.join(
-                ['{}: {}'.format(format_substitutions(k), format_substitutions(v))  # type:ignore[misc]
+                ['{}: {}'.format(format_substitutions(k),  # type:ignore[misc]
+                                 format_substitutions(v))
                  for k, v in typed_action.env]
                  ) + '}',
             typed_action.shell,
