2ba8c6be7cb266221f3a3707ac00a259b0fcc6a2
Christophe Bedard <bedard.christophe@gmail.com>
2025-04-28 13:23:09 -0700

    Revert "Add Other Logging Implementations (#858)"
    This reverts commit b7b31c45b0eb350deedd282b88398d1ca0d5faf4.

--- launch_yaml/test/launch_yaml/test_for_loop.py
+++ launch_yaml/test/launch_yaml/test_for_loop.py
@@ -39,7 +39,7 @@ def test_for_each():
             - for_each:
                 values: $(var robots)
                 children:
-                    - log_info:
+                    - log:
                         message: "'$(for-var name)' id=$(for-var id 0) ($(for-var opt 'none'))"
         """
     )
@@ -111,7 +111,7 @@ def test_for_loop():
                 len: $(var num_i)
                 name: i
                 children:
-                    - log_info:
+                    - log:
                         message: index=$(index i)
         """
     )
