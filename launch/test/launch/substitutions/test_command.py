c4f713ed93e493a07166de53162888a172f37a84
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2025-05-15 08:35:51 -0700

    Update launch/test/launch/substitutions/test_command.py
    Co-authored-by: Christophe Bedard <bedard.christophe@gmail.com>
    Signed-off-by: Emerson Knapp <537409+emersonknapp@users.noreply.github.com>

--- launch/test/launch/substitutions/test_command.py
+++ launch/test/launch/substitutions/test_command.py
@@ -36,7 +36,7 @@ def commands():
 
     if os.name == 'nt':
         for key, value in commands.items():
-            commands[key] = value.with_suffix('bat')
+            commands[key] = value.with_suffix('.bat')
     return commands
 
 
