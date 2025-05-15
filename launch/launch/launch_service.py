3afcb007ad6acfd276d39afb7b2434fb466d7949
Michael Carlstrom <rmc@carlstrom.com>
2025-05-15 11:08:42 -0400

    Update launch/launch/launch_service.py
    Co-authored-by: Christophe Bedard <bedard.christophe@gmail.com>
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- launch/launch/launch_service.py
+++ launch/launch/launch_service.py
@@ -217,7 +217,7 @@ class LaunchService:
                 # Setup signal handlers
                 manager.handle(signal.SIGINT, _on_sigint)
                 manager.handle(signal.SIGTERM, _on_sigterm)
-                if sys.platform != 'win322':
+                if sys.platform != 'win32':
                     manager.handle(signal.SIGQUIT, _on_sigterm)
                 # Yield asyncio loop and current task.
                 yield this_loop, this_task
