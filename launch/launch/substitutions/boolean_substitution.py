8a23f34567d17a6e5364a91b613295d9947af2cc
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2025-10-06 14:09:40 +0200

    Fix all/any in xml and yaml launch files (#906)
    Signed-off-by: Matthijs van der Burgh <MatthijsBurgh@outlook.com>
    (cherry picked from commit 835d1368a2fc28f5262319de7638aa2c25dbc791)
    
    # Conflicts:
    #	launch/launch/substitutions/boolean_substitution.py

--- launch/launch/substitutions/boolean_substitution.py
+++ launch/launch/substitutions/boolean_substitution.py
@@ -14,7 +14,17 @@
 
 """Module for boolean substitutions."""
 
+<<<<<<< HEAD
 from typing import Iterable
+=======
+from itertools import chain
+from typing import Any
+from typing import Dict
+from typing import Iterable
+from typing import List
+from typing import Optional
+from typing import Sequence
+>>>>>>> 835d136 (Fix all/any in xml and yaml launch files (#906))
 from typing import Text
 
 from .substitution_failure import SubstitutionFailure
@@ -162,7 +172,11 @@ class AnySubstitution(Substitution):
     If none of the arguments evaluate to true, then this substitution returns the string 'false'.
     """
 
-    def __init__(self, *args: SomeSubstitutionsType) -> None:
+    def __init__(
+        self,
+        *args: SomeSubstitutionsType,
+        container: Optional[Iterable[SomeSubstitutionsType]] = None,
+    ) -> None:
         """
         Create an AnySubstitution substitution.
 
@@ -170,12 +184,15 @@ class AnySubstitution(Substitution):
         """
         super().__init__()
 
-        self.__args = [normalize_to_list_of_substitutions(arg) for arg in args]
+        if container is None:
+            container = []
+
+        self.__args = [normalize_to_list_of_substitutions(arg) for arg in chain(args, container)]
 
     @classmethod
     def parse(cls, data: Iterable[SomeSubstitutionsType]):
         """Parse `AnySubstitution` substitution."""
-        return cls, {'args': data}
+        return cls, {'container': data}
 
     @property
     def args(self) -> Substitution:
@@ -207,7 +224,11 @@ class AllSubstitution(Substitution):
     If any of the arguments evaluates to false, then this substitution returns the string 'false'.
     """
 
-    def __init__(self, *args: SomeSubstitutionsType) -> None:
+    def __init__(
+        self,
+        *args: SomeSubstitutionsType,
+        container: Optional[Iterable[SomeSubstitutionsType]] = None,
+    ) -> None:
         """
         Create an AllSubstitution substitution.
 
@@ -216,12 +237,15 @@ class AllSubstitution(Substitution):
         """
         super().__init__()
 
-        self.__args = [normalize_to_list_of_substitutions(arg) for arg in args]
+        if container is None:
+            container = []
+
+        self.__args = [normalize_to_list_of_substitutions(arg) for arg in chain(args, container)]
 
     @classmethod
     def parse(cls, data: Iterable[SomeSubstitutionsType]):
         """Parse `AllSubstitution` substitution."""
-        return cls, {'args': data}
+        return cls, {'container': data}
 
     @property
     def args(self) -> Substitution:
