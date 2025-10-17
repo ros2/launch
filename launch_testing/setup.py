91c9bd581fb1d2cf75cce8655fea76f715a43934
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-17 12:03:47 +0200

    3.4.8

--- launch_testing/setup.py
+++ launch_testing/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='launch_testing',
-    version='3.4.7',
+    version='3.4.8',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/launch_testing']),
