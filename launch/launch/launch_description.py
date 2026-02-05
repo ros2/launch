3f14b15e76714c1a913d635a9815520f53465c41
YuxuanLiuTier4Desktop <619684051@qq.com>
2026-02-05 13:15:44 +0900

    re-format
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

--- launch/launch/launch_description.py
+++ launch/launch/launch_description.py
@@ -131,8 +131,12 @@ class LaunchDescription(LaunchDescriptionEntity):
             Tuple[DeclareLaunchArgument, List[IncludeLaunchDescription]]] = []
         from .actions import ResetLaunchConfigurations
 
-        def process_entities(entities, *, _conditional_inclusion: bool, nested_ild_actions=None,
-                                only_search_local: bool = False):
+        def process_entities(
+                entities,
+                *,
+                _conditional_inclusion: bool,
+                nested_ild_actions=None,
+                only_search_local: bool = False):
             for entity in entities:
                 if isinstance(entity, DeclareLaunchArgument):
                     # Avoid duplicate entries with the same name.
